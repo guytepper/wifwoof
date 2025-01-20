c360264035eff01e01ee6b2bff714c8643a79d1f
Guy Tepper <hey@guytepper.com>
2025-01-20 08:07:18 +0200

    ♻️ - Extract dog animation config to a hook

--- src/App.tsx
+++ src/App.tsx
@@ -13,9 +13,38 @@ const Price = motion.create(NumberFlow);
 function App() {
   const [isSoundOn, setIsSoundOn] = useState(true);
   const { price, bgColor, shouldPopHappy, shouldPopSad } = usePrice({ mute: !isSoundOn });
+  const animateDog = useAnimateDog(shouldPopHappy, shouldPopSad);
+
   useThemeColor(bgColor);
 
-  const animateDog = useMemo(() => {
+  return (
+    <div className="App" style={{ backgroundColor: bgColor, transition: "background-color 0.6s" }}>
+      <header>
+        <motion.div
+          whileHover={{ scale: 1.2, origin: "center", cursor: "pointer" }}
+          onClick={() => setIsSoundOn(!isSoundOn)}
+        >
+          {isSoundOn ? <SoundOffIcon width={40} height={40} /> : <SoundOnIcon width={40} height={40} />}
+        </motion.div>
+      </header>
+
+      <Price value={price} format={PRICE_FORMAT} plugins={[continuous]} className="price" style={{ y: "-26vh" }} />
+
+      <motion.div
+        style={{ position: "absolute", y: "50vh" }}
+        animate={animateDog}
+        whileHover={{ scale: 1.1, y: "45vh" }}
+      >
+        <img src={dog} alt="dogwifhat" className="dogwifhat" />
+      </motion.div>
+    </div>
+  );
+}
+
+export default App;
+
+const useAnimateDog = (shouldPopHappy: boolean, shouldPopSad: boolean) => {
+  return useMemo(() => {
     if (shouldPopHappy) {
       return {
         scale: 1.25,
@@ -36,33 +65,4 @@ function App() {
       };
     }
   }, [shouldPopHappy, shouldPopSad]);
-
-  return (
-    <div className="App" style={{ backgroundColor: bgColor, transition: "background-color 0.6s" }}>
-      <header>
-        <motion.div
-          whileHover={{ scale: 1.2, origin: "center", cursor: "pointer" }}
-          onClick={() => setIsSoundOn(!isSoundOn)}
-        >
-          {isSoundOn ? <SoundOffIcon width={40} height={40} /> : <SoundOnIcon width={40} height={40} />}
-        </motion.div>
-      </header>
-
-      <Price value={price} format={PRICE_FORMAT} plugins={[continuous]} className="price" style={{ y: "-26vh" }} />
-
-      <motion.div
-        style={{ position: "absolute", y: "50vh" }}
-        animate={animateDog}
-        whileHover={{ scale: 1.1, y: "45vh" }}
-      >
-        <img
-          src={dog}
-          alt="dogwifhat"
-          style={{ width: "300px", height: "300px", userSelect: "none", pointerEvents: "none" }}
-        />
-      </motion.div>
-    </div>
-  );
-}
-
-export default App;
+};
