bdc0817439b37e55432bdeabf34c5b9ac9c448e4
Guy Tepper <hey@guytepper.com>
2025-01-24 18:55:58 +0200

    🔧 - Add firebase config

---
+++ src/config/firebase-config.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getDatabase(app);
+export default app;
